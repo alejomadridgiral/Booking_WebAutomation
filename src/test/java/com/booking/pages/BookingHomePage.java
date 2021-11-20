efe5e97f250436e78b35296b78f27420a6c2729a
Alejo Madrid <alejandro.madrid@globant.com>
2021-11-19 19:04:47 -0500

    check in date working

--- src/test/java/com/booking/pages/BookingHomePage.java
+++ src/test/java/com/booking/pages/BookingHomePage.java
@@ -10,27 +10,88 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BookingHomePage extends BasePage {
 
-    public BookingHomePage(WebDriver driver){
+    public BookingHomePage(WebDriver driver) {
         super(driver);
         driver.get("https://www.booking.com/index.es.html");
     }
 
 
-    @FindBy(css="li a[class='bui-tab__link bui-tab__link--selected']")
+    @FindBy(css = "li a[class='bui-tab__link bui-tab__link--selected']")
     private WebElement stays;
 
-    public void clickStays(){
+    public void clickStays() {
         getWait().until(ExpectedConditions.elementToBeClickable(stays));
         stays.click();
     }
 
-    @FindBy(id="ss")
+    @FindBy(id = "ss")
     private WebElement searchCity;
 
-    public void fillSearchCity(String city){
+    public void fillSearchCity(String city) {
         getWait().until(ExpectedConditions.elementToBeClickable(searchCity));
         searchCity.click();
         searchCity.sendKeys(city);
     }
+
+    @FindBy(className = "sb-date-field__field")
+    private WebElement checkInDate;
+
+    public void clickCheckInData() {
+        checkInDate.click();
+    }
+
+    @FindBy(className = "bui-calendar__dates")
+    private WebElement calendar;
+
+    public void visibilityOfCalendar() {
+        getWait().until(ExpectedConditions.visibilityOfAllElements(calendar));
+    }
+
+    @FindBy(xpath = "//div[normalize-space()='noviembre 2021']")
+    private WebElement checkInMonthAndYear;
+
+    public String getCheckInMonthAndYear() {
+        return checkInMonthAndYear.getText();
+    }
+
+    public String getCheckInMonth() {
+        getWait().until(ExpectedConditions.visibilityOfAllElements(calendar));
+        return getCheckInMonthAndYear().split(" ")[0].trim();
+    }
+
+    public String getCheckInYear() {
+        getWait().until(ExpectedConditions.visibilityOfAllElements(calendar));
+        return getCheckInMonthAndYear().split(" ")[1].trim();
+    }
+
+    @FindBy (xpath = "//div[@class='bui-calendar__control bui-calendar__control--next']//*[name()='svg']")
+    private WebElement nextMonth;
+
+    public void clickNextMonth(){
+        getWait().until(ExpectedConditions.visibilityOfAllElements(calendar));
+        nextMonth.click();
+    }
+
+    @FindBy(css = "span[aria-label='25 noviembre 2021'] span[aria-hidden='true']")
+    private WebElement calendarDay;
+
+    //td[@class='bui-calendar__date bui-calendar__date--selected']
+
+
+
+    public void clickCalendarDay(){
+        getWait().until(ExpectedConditions.visibilityOfAllElements(calendar));
+        calendarDay.click();
+    }
+
+    public void lookForFuture(){
+        while(!(getCheckInMonth().equals("June") && getCheckInYear().equals("2022"))){
+            nextMonth.click();
+        }
+    }
+
+    @FindBy (xpath = "//div[normalize-space()='diciembre 2021']")
+    private WebElement checkOutMonth;
+
 
 }
--- src/test/java/com/booking/test/BookingHomeTest.java
+++ src/test/java/com/booking/test/BookingHomeTest.java
@@ -13,8 +13,22 @@ public class BookingHomeTest extends BaseTest{
         BookingHomePage bookingHome = getBookingHomePage();
         bookingHome.clickStays();
         bookingHome.fillSearchCity("Bogota, Colombia");
-
+        bookingHome.clickCheckInData();
+        bookingHome.visibilityOfCalendar();
+        bookingHome.getCheckInMonthAndYear(); //String MonthAndYear = bookingHome.getCheckInMonthAndYear();
+//        System.out.println(MonthAndYear);
+//        String month = bookingHome.getCheckInMonth();
+//        System.out.println(month);
+//        String year = bookingHome.getCheckInYear();
+//        System.out.println(year);
+//        while(!(month.equals("June") && year.equals("2022"))){
+//            MonthAndYear = bookingHome.getCheckInMonthAndYear();
+//            bookingHome.clickNextMonth();
+//            month = bookingHome.getCheckInMonth();
+//            year = bookingHome.getCheckInYear();
+//        }
+////        bookingHome.lookForFuture();
+        bookingHome.clickCalendarDay();
     }
-
 
 }
